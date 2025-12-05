b867c09e81ec2fbb961c2cf8599758952770bf73
Vasanthraj-vr31 <vasanthrajj2005@gmail.com>
2025-12-05 10:26:01 +0530

    5.0

---
+++ calc.js
@@ -0,0 +1,110 @@
+const enter1 = () => {
+    const heading = document.createElement("h1");
+    const data = document.getElementById("one").innerText;
+    heading.innerText = data;
+    const cont = document.getElementById("display");
+    cont.append(heading);
+}
+
+const enter2 = () => {
+    const heading = document.createElement("h1");
+    const data = document.getElementById("two").innerText;
+    heading.innerText = data;
+    const cont = document.getElementById("display");
+    cont.append(heading);
+}
+const enter3 = () => {
+    const heading = document.createElement("h1");
+    const data = document.getElementById("th").innerText;
+    heading.innerText = data;
+    const cont = document.getElementById("display");
+    cont.append(heading);
+}
+const enter4 = () => {
+    const heading = document.createElement("h1");
+    const data = document.getElementById("fo").innerText;
+    heading.innerText = data;
+    const cont = document.getElementById("display");
+    cont.append(heading);
+}
+const enter5 = () => {
+    const heading = document.createElement("h1");
+    const data = document.getElementById("fi").innerText;
+    heading.innerText = data;
+    const cont = document.getElementById("display");
+    cont.append(heading);
+}
+const enter6= () => {
+    const heading = document.createElement("h1");
+    const data = document.getElementById("si").innerText;
+    heading.innerText = data;
+    const cont = document.getElementById("display");
+    cont.append(heading);
+}
+const enter7 = () => {
+    const heading = document.createElement("h1");
+    const data = document.getElementById("se").innerText;
+    heading.innerText = data;
+    const cont = document.getElementById("display");
+    cont.append(heading);
+}
+const enter8 = () => {
+    const heading = document.createElement("h1");
+    const data = document.getElementById("ei").innerText;
+    heading.innerText = data;
+    const cont = document.getElementById("display");
+    cont.append(heading);
+}
+const enter9 = () => {
+    const heading = document.createElement("h1");
+    const data = document.getElementById("ni").innerText;
+    heading.innerText = data;
+    const cont = document.getElementById("display");
+    cont.append(heading);
+}
+
+const enter0 = () => {
+    const heading = document.createElement("h1");
+    const data = document.getElementById("te").innerText;
+    heading.innerText = data;
+    const cont = document.getElementById("display");
+    cont.append(heading);
+}
+const plus= () => {
+    const heading = document.createElement("h1");
+    const data = document.getElementById("pl").innerText;
+    heading.innerText = data;
+    const cont = document.getElementById("display");
+    cont.append(heading);
+}
+const minus = () => {
+    const heading = document.createElement("h1");
+    const data = document.getElementById("mi").innerText;
+    heading.innerText = data;
+    const cont = document.getElementById("display");
+    cont.append(heading);
+}
+
+const mult = () => {
+    const heading = document.createElement("h1");
+    const data = document.getElementById("mu").innerText;
+    heading.innerText = data;
+    const cont = document.getElementById("display");
+    cont.append(heading);
+}
+
+const divi= () => {
+    const heading = document.createElement("h1");
+    const data = document.getElementById("di").innerText;
+    heading.innerText = data;
+    const cont = document.getElementById("display");
+    cont.append(heading);
+}
+
+const equa= () => {
+    const heading = document.createElement("h1");
+    const data = document.getElementById("eq").innerText;
+    heading.innerText = data;
+    const cont = document.getElementById("display");
+    cont.append(heading);
+}
---
+++ index.js
@@ -0,0 +1,12 @@
+/*const arr=()=>{
+    const heading=document.createElement("h1");
+    heading.innerText="Vasanthraj";
+    const cont=document.getElementById("container");
+    cont.append(heading);
+}*/
+
+
+const col=function(){
+    const div=document.getElementById("cont")
+    div.style.backgroundColor="black"
+}
